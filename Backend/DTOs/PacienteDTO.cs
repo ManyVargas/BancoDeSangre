54b70df01128bb515a2643bcfdf3562184192748
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 00:41:59 -0400

    Add PacienteDTO

---
+++ Backend/DTOs/PacienteDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DTOs
+{
+    public class PacienteDTO
+    {
+        public int PacienteId { get; set; }
+        public string Nombre { get; set; }
+        public string CedulaID { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public int TipoSangreID { get; set; }
+    }
+
+    public class RegistrarPacienteDTO
+    {
+        public string Nombre { get; set; }
+        public string CedulaID { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public int TipoSangreID { get; set; }
+    }
+
+    public class ListarPacienteDTO
+    {
+        public int PacienteId { get; set; }
+        public string Nombre { get; set; }
+        public string CedulaID { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string TipoSangre { get; set; }
+    }
+}
