7d0101d9c791f3e44de16de77a7cdd81e933d625
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 18:39:08 -0400

    Add IHospitalRepositorio

---
+++ Backend/Interfaces/IHospitalRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.DTOs;
+
+namespace Backend.Interfaces
+{
+    public interface IHospitalRepositorio
+    {
+        Task<IEnumerable<HospitalDTO>> ObtenerTodosLosHospitales(CancellationToken ct);
+        Task<HospitalDTO> ObtenerHospitalPorNombre(string NombreHospital, CancellationToken ct);
+        Task<int> RegistrarHospital(RegistrarHospitalDTO nuevoHospital, CancellationToken ct);
+        Task<HospitalDTO> ActualizarHospital(int hospitalId,RegistrarHospitalDTO hospitalActualizado, CancellationToken ct);
+        Task<bool> EliminarHospital(int hospitalId, CancellationToken ct);
+
+    }
+}
