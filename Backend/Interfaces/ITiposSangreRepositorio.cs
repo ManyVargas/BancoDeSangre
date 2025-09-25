a1dfb9ad0bfdff1b342109658f27de8d487d6a23
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 12:08:05 -0400

    Add ITipoDeSangreRepositorio

---
+++ Backend/Interfaces/ITiposSangreRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.DTOs;
+
+namespace Backend.Interfaces
+{
+    public interface ITiposSangreRepositorio
+    {
+        Task<IEnumerable<TiposSangreDTO>> ListarTiposSangreAsync(CancellationToken ct);
+    }
+}
