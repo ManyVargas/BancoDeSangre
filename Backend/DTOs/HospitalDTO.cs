ebe5b922056daea3ab7b4a31f6d8c39769ef3308
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 18:37:55 -0400

    Add HospitalDTO

---
+++ Backend/DTOs/HospitalDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DTOs
+{
+    public class HospitalDTO
+    {
+        public int HospitalID { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public decimal? Latitud { get; set; }
+        public decimal? Longitud { get; set; }
+        public string ContactoPrincipal { get; set; }
+        public string EmailContacto { get; set; }
+    }
+
+    public class RegistrarHospitalDTO
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public decimal? Latitud { get; set; }
+        public decimal? Longitud { get; set; }
+        public string ContactoPrincipal { get; set; }
+        public string EmailContacto { get; set; }
+    }
+}
