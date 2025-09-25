36da3ecf2b98c26c6a69218a4f169b7e92c20dec
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 19:02:00 -0400

    Add IResultadosPruebaRepositorio

---
+++ Backend/Interfaces/IResultadosPrueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Interfaces
+{
+    public interface IResultadosPrueba
+    {
+        Task<IEnumerable<DTOs.ResultadosPruebaDTO>> ListarResultadosPruebas(CancellationToken ct);
+    }
+}
