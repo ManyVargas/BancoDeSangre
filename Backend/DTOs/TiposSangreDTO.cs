c27ac59238a4485322ce7199db3d95e92058f911
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 12:07:45 -0400

    Add TipoDeSangreDTO

---
+++ Backend/DTOs/TiposSangreDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DTOs
+{
+    public class TiposSangreDTO
+    {
+        public int TipoSangreID { get; set; }
+        public string Tipo { get; set; }
+    }
+}
