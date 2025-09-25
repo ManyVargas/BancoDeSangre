45a757e41b74dd59aacb213322572d106675286a
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 00:42:14 -0400

    Add IPacienteRepostorio

---
+++ Backend/Interfaces/IPacienteRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.DTOs;
+
+namespace Backend.Interfaces
+{
+    internal interface IPacienteRepositorio
+    {
+        Task<IEnumerable<ListarPacienteDTO>> ListarPacientePacientesAsync(CancellationToken ct);
+        Task<IEnumerable<ListarPacienteDTO>> ListarPacientePacientesPorTipoDeSangreAsync(string TipoSangre, CancellationToken ct);
+        Task<ListarPacienteDTO> ObtenerPacientePorIdAsync(int id, CancellationToken ct);
+        Task<int> RegistrarPacienteAsync(PacienteDTO paciente, CancellationToken ct);
+        Task<ListarPacienteDTO?> ActualizarPacienteAsync(PacienteDTO paciente, CancellationToken ct);
+        Task<bool> EliminarPacienteAsync(int id, CancellationToken ct);
+    }
+}
