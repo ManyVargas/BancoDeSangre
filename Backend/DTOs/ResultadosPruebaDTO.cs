78d6a28df575eb094f0f74a1dde7b1343f16fa59
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-25 19:01:45 -0400

    Add ResultadosPruebaDTO

---
+++ Backend/DTOs/ResultadosPruebaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DTOs
+{
+    public class ResultadosPruebaDTO
+    {
+        public int ResultadoID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+
+}
