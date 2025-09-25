805727ed49eeae5f7f014b2aa24756e115a983bf
Est - IDS Coste Alvarez, Frank Delvys <1121598@est.intec.edu.do>
2025-09-24 20:06:56 -0400

    Add IDonanteRepositorio

---
+++ Backend/Interfaces/IDonanteRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.DTOs;
+
+namespace Backend.Interfaces
+{
+    public interface IDonanteRepositorio
+    {
+        Task<IEnumerable<DonantesDTO>> ListarAsync(CancellationToken ct);
+        Task<IEnumerable<DonantesDTO>> ListarPorTipoSangreAsync(string nombre, CancellationToken ct);
+        Task<DonantesDTO?> ObtenerPorIdAsync(int id, CancellationToken ct); 
+        Task<int> CrearAsync(RegistrarDonanteDTO dto, CancellationToken ct);
+        Task<DonantesDTO?> ActualizarAsync(int id, ActualizarDonanteDTO dto, CancellationToken ct);
+        Task<bool> EliminarAsync(int id, CancellationToken ct);
+    }
+}
